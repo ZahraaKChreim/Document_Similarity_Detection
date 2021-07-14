7cc86dfe7ab65ae8df743c916c5e9deffe11381f
zahraachreim <zahraa.karim.chreim@outlook.com>
2021-07-14 18:04:28 +0300

    first commit

--- document.py
+++ document.py
@@ -1,54 +1,42 @@
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 
 def get_webpage_content(driver, url):
 
-    #######################################################
-    ################# Getting the website #################
-    #######################################################
-
-    #print("Getting the Website ...")
     driver.get(url)
-    #print("Getting the Website Done")
-    #print("-------------------------------------------------------------------------------------------------")
+    website_title = (driver.title).lower()
 
-    #######################################################
-    ############### Getting the Page's Title ##############
-    #######################################################
-
-    #print("Getting the Page's Title ...")
-    ## page_title = first h1 element
+    ## Page_title = first h1 element
     try:
         first_h1_element = driver.find_element_by_xpath("//h1")
-        page_title = first_h1_element.text
-        #print("Page Title: ",page_title)
+        page_title = (first_h1_element.text).lower()
     except NoSuchElementException:
         page_title = ""
-        print("NoSuchElementException - no <h1> element - no Page Title")
         pass
-    #print("Getting the Page's Title Done")
-    #print("-------------------------------------------------------------------------------------------------")
 
+    ## Subtitles
+    h16_elements = driver.find_elements_by_xpath("//h1/*[1] | //h2/*[1] | //h3/*[1] | //h4/*[1]")
+    h16_elements = h16_elements[1:]
+    list_of_subtitles = []
+    for subtitle in h16_elements:
+        if subtitle.text:
+            list_of_subtitles.append((subtitle.text).lower())
 
-    #######################################################
-    ##### Getting the Page's Non Empty Text Paragraphs ####
-    #######################################################
-
-    #print("Getting the Page's Non Empty Text Paragraphs ...")
+    ## Body Text Paragraphs
     list_of_paragraphs = driver.find_elements_by_xpath("//p")
-    #print("list_of_paragraphs length: ",len(list_of_paragraphs))
     list_of_text_paragraphs = []
     for paragraph in list_of_paragraphs:
         if paragraph.text:
             list_of_text_paragraphs.append(paragraph.text)
-            #print("Tag: ",paragraph.tag_name," - Text: ",paragraph.text)
-            #print("#################################")
-    #print("list_of_text_paragraphs length: ",len(list_of_text_paragraphs))
     text_paragraphs_single_string = ' '.join(str(paragraph) for paragraph in list_of_text_paragraphs)
-    #print("Getting the Page's Non Empty Text Paragraphs Done ...")
-    #print("-------------------------------------------------------------------------------------------------")
 
-    return page_title, list_of_text_paragraphs, text_paragraphs_single_string
+    ## URLs
+    list_of_links_a = driver.find_elements_by_xpath('//p/a | //ul/li/*/a | //ul/li/a | //ol/li/a')
+    list_of_urls = []
+    for link in list_of_links_a:
+        try:
+            if link.get_attribute('href') and link.get_attribute('href').startswith("http") and link.text:
+                list_of_urls.append(link.get_attribute('href'))
+        except StaleElementReferenceException:
+            pass
+
+    return website_title, page_title, list_of_subtitles, list_of_urls, text_paragraphs_single_string
